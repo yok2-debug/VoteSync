64ec80b22c70ab6776295c6dd55af0b9cde57e54
Waluyo Padmobesari <yoyokk@gmail.com>
2025-11-20 01:48:07 +0000

    pada halaman /vote/-OePaH9qWt1TFBOnUvm5 kurangi padding bawah gambar kan

--- src/app/vote/[electionId]/page.tsx
+++ src/app/vote/[electionId]/page.tsx
@@ -94,7 +94,7 @@ function VotePageContent() {
         <div className="grid justify-center gap-6 md:grid-cols-2 lg:grid-cols-3">
           {candidates.map(candidate => (
               <Card key={candidate.id} className="flex flex-col">
-              <CardHeader className="items-center text-center p-6">
+              <CardHeader className="items-center text-center p-4 pb-1 space-y-1">
                   <img
                       src={candidate.photo || defaultPhoto?.imageUrl || 'https://picsum.photos/seed/default/400/400'}
                       alt={`Photo of ${candidate.name}`}
@@ -112,7 +112,7 @@ function VotePageContent() {
                     )}
                   </CardTitle>
               </CardHeader>
-              <CardContent className="flex-grow flex flex-col justify-center items-center px-6 py-2">
+              <CardContent className="flex-grow flex flex-col justify-center items-center px-4 py-2">
                   <Dialog>
                       <DialogTrigger asChild>
                       <Button variant="secondary">
@@ -141,7 +141,7 @@ function VotePageContent() {
                       </DialogContent>
                   </Dialog>
               </CardContent>
-              <div className="p-6 pt-4">
+              <div className="p-4 pt-1">
                   <CandidateVoteForm electionId={election.id} candidate={candidate} voterId={session.voterId!} />
               </div>
               </Card>
