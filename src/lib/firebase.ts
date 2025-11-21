0f881d843102f1bc54206af380ea3dbe1bdb394a
Waluyo Padmobesari <yoyokk@gmail.com>
2025-11-21 14:48:22 +0000

    saya akan deploy firebase hosting site "evoting-cfb3e"

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,10 +3,10 @@ import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAdS878XeSy9Dc2pU5UhiOKN0ZJp5RH_oA",
-  authDomain: "e-voting-68dda.firebaseapp.com",
-  databaseURL: "https://e-voting-68dda-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "e-voting-68dda",
-  storageBucket: "e-voting-68dda.firebasestorage.app",
+  authDomain: "evoting-cfb3e.firebaseapp.com",
+  databaseURL: "https://evoting-cfb3e-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "evoting-cfb3e",
+  storageBucket: "evoting-cfb3e.appspot.com",
   messagingSenderId: "786875940790",
   appId: "1:786875940790:web:243f2c27fd747cb54d0b17",
 };
