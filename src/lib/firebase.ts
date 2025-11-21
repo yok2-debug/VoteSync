802bb1286ea7700270cd120bd1e2931a8558980b
Waluyo Padmobesari <yoyokk@gmail.com>
2025-11-21 15:48:25 +0000

    konfigurasi database : const firebaseConfig = {   apiKey: "AIzaSyAdS878X

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,11 +4,12 @@ import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAdS878XeSy9Dc2pU5UhiOKN0ZJp5RH_oA",
-  authDomain: "evoting-cfb3e.firebaseapp.com",
-  projectId: "evoting-cfb3e",
-  storageBucket: "evoting-cfb3e.appspot.com",
+  authDomain: "e-voting-68dda.firebaseapp.com",
+  databaseURL: "https://e-voting-68dda-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "e-voting-68dda",
+  storageBucket: "e-voting-68dda.appspot.com",
   messagingSenderId: "786875940790",
-  appId: "1:786875940790:web:243f2c27fd747cb54d0b17",
+  appId: "1:786875940790:web:243f2c27fd747cb54d0b17"
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
