9a294a3e071bc280a00f0f34a942d51ce103fe29
Waluyo Padmobesari <yoyokk@gmail.com>
2025-11-21 15:45:55 +0000

    masih belum update ke database

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,10 @@
+'use client';
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
   apiKey: "AIzaSyAdS878XeSy9Dc2pU5UhiOKN0ZJp5RH_oA",
   authDomain: "evoting-cfb3e.firebaseapp.com",
-  databaseURL: "https://evoting-cfb3e-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "evoting-cfb3e",
   storageBucket: "evoting-cfb3e.appspot.com",
   messagingSenderId: "786875940790",
