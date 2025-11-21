b0adb63dde1dbb74940046e9a1d849d01886813f
Waluyo Padmobesari <yoyokk@gmail.com>
2025-11-21 16:39:21 +0000

    // Your web app's Firebase configuration const firebaseConfig = {   apiK

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,16 +2,17 @@
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
-// Corrected Firebase Configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA_I47uV2-3s9b6c8d5a2f1g7h3j5k9l3",
+  apiKey: "AIzaSyAdS878XeSy9Dc2pU5UhiOKN0ZJp5RH_oA",
   authDomain: "e-voting-68dda.firebaseapp.com",
   databaseURL: "https://e-voting-68dda-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "e-voting-68dda",
-  storageBucket: "e-voting-68dda.appspot.com",
+  storageBucket: "e-voting-68dda.firebasestorage.app",
   messagingSenderId: "786875940790",
   appId: "1:786875940790:web:243f2c27fd747cb54d0b17"
 };
+
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getDatabase(app);
