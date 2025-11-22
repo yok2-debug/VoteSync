de62fc88eccd07cfeba932b8bf5f8dd7e1dbd5de
Waluyo Padmobesari <yoyokk@gmail.com>
2025-11-22 16:22:05 +0000

    tambahkan footer yang sebelumnya juga

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -5,10 +5,13 @@ import * as React from 'react';
 export function Footer() {
   return (
     <footer className="py-6 md:px-8 md:py-0 bg-background/95 backdrop-blur-sm border-t">
-      <div className="container flex flex-col items-center justify-center gap-4 md:h-24 md:flex-row">
-        <p className="text-balance text-center text-sm leading-loose text-muted-foreground">
-          Created by Waluyo
-        </p>
+      <div className="container flex flex-col items-center justify-center gap-2 md:h-24 md:flex-row">
+        <div className="text-balance text-center text-sm leading-loose text-muted-foreground">
+          <p>© 2024 VoteSync. All rights reserved.</p>
+          <p>
+            Created by Waluyo
+          </p>
+        </div>
       </div>
     </footer>
   );
